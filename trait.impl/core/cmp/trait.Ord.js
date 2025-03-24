f0216a6ce4118ea4c0b60e1eb3c7613e04885f88
Bastacyclop <thomas.koehler@thok.eu>
2025-03-24 16:34:44 +0000

    Deploying to gh-pages from @ Bastacyclop/egg-sketches@233e8484b176755723bb51839c81c89b2f8843a6 🚀

--- trait.impl/core/cmp/trait.Ord.js
+++ trait.impl/core/cmp/trait.Ord.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["egg_sketches",[["impl&lt;L: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"egg_sketches/enum.SketchNode.html\" title=\"enum egg_sketches::SketchNode\">SketchNode</a>&lt;L&gt;"]]]]);
+    var implementors = Object.fromEntries([["egg_sketches",[["impl&lt;L: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"egg_sketches/enum.SketchNode.html\" title=\"enum egg_sketches::SketchNode\">SketchNode</a>&lt;L&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/marker/trait.StructuralPartialEq.js
+++ trait.impl/core/marker/trait.StructuralPartialEq.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["egg_sketches",[["impl&lt;L&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"egg_sketches/enum.SketchNode.html\" title=\"enum egg_sketches::SketchNode\">SketchNode</a>&lt;L&gt;"]]]]);
+    var implementors = Object.fromEntries([["egg_sketches",[["impl&lt;L&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"egg_sketches/enum.SketchNode.html\" title=\"enum egg_sketches::SketchNode\">SketchNode</a>&lt;L&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
